6b264ff098ab93bd08d31097bfd7a9bd62743697
mikoloism <mikoloism.github@gmail.com>
2021-04-30 21:24:37 +0430

    Importing Modules (Initial 👍)

--- hjs/hjs.js
+++ hjs/hjs.js
@@ -1,9 +1,16 @@
-import Render from './utils/render';
-import Compile from './utils/compile';
+import Render from './render';
+import Compiler from './compiler';
+import createHjs from './create-hjs';
+import JOM from './jom';
+import DOM from './dom';
+import utils from './utils';
+
+// JSOM || JOM => JavaScript-Object-Model
+// actullay, alike DOM but only fetch hjs objects
 
 const pson = (objects, element) => {
 	let HTMLPson = render(objects, element);
 	return HTMLPson;
 };
 
-export default pson;
+export default { Render, Compiler, createHjs, DOM, JOM, utils };
