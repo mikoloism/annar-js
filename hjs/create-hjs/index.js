4f234473989dd8fc6ba2cd4255c01a2c2af61152
mikoloism <mikoloism.github@gmail.com>
2021-04-30 21:24:01 +0430

    initial model 2

---
+++ hjs/create-hjs/index.js
@@ -0,0 +1,6 @@
+import createHjs from './element.js';
+import { Component, createComponent } from './component.js';
+
+// hjsComponent || createComponent (may createHjs instead)
+export { Component, createComponent };
+export default createHjs;
