fcfd77bce05e74291468aaa209e468c6a70b7056
mikoloism <mikoloism.github@gmail.com>
2021-05-02 01:27:13 +0430

    Fix

--- hjs/index.js
+++ hjs/index.js
@@ -1,9 +1,9 @@
 import render from './render';
-import renderFile from './compiler';
+import renderToFile from './compiler';
 import createHjs, { Component, createComponent } from './create-hjs';
 import * as JOM from './jom';
 import * as DOM from './dom';
 import utils from './utils';
 
-export { renderFile, createHjs, Component, createComponent, DOM, JOM, utils };
+export { renderToFile, createHjs, Component, createComponent, DOM, JOM, utils };
 export default render;
