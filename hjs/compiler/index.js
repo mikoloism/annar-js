3b76ca5d25a68541c74574b19a5c48b76185f4be
mikoloism <mikoloism.github@gmail.com>
2021-05-02 00:33:48 +0430

    Fixing dynamic render file path, and Adding MountPointElement

--- hjs/compiler/index.js
+++ hjs/compiler/index.js
@@ -1,12 +1,10 @@
 import fs from 'fs';
 import render from '../render';
 
-const compiler = function RenderToFile(
-	HJSObject,
-	filePath = '../../public/index.html',
-) {
+const compiler = function RenderToFile(HJSObject, RootElement) {
+	let filePath = '../../public/index.html';
 	let HTMLObject = render(HJSObject);
-	fs.writeFileSync(FilePath, HTMLObject.outerHTML, 'utf8');
+	fs.writeFileSync(filePath, HTMLObject.outerHTML, 'utf8');
 };
 
 export default compiler;
