3be8b968255bbbcea9c2c884791177ac0311ddc2
mikoloism <mikoloism.github@gmail.com>
2021-05-01 06:51:35 +0430

    Delete index.js, Rename hjs.js to index.js

--- hjs/index.js
+++ hjs/index.js
@@ -1,3 +1,8 @@
-import hjs from './hjs';
-
-export default hjs;
+import render from './render';
+import renderFile from './compiler';
+import createHjs, { Component, createComponent } from './create-hjs';
+import * as JOM from './jom';
+import * as DOM from './dom';
+import utils from './utils';
+export { renderFile, createHjs, Component, createComponent, DOM, JOM, utils };
+export default render;
