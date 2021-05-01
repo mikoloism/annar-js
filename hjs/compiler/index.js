80cde0377f4dd966389381e71adbb8203c179c64
mikoloism <mikoloism.github@gmail.com>
2021-05-02 00:29:35 +0430

    create RenderToFile

--- hjs/compiler/index.js
+++ hjs/compiler/index.js
@@ -1,3 +1,12 @@
-const compiler = function RenderToFile() {};
+import fs from 'fs';
+import render from '../render';
+
+const compiler = function RenderToFile(
+	HJSObject,
+	filePath = '../../public/index.html',
+) {
+	let HTMLObject = render(HJSObject);
+	fs.writeFileSync(FilePath, HTMLObject.outerHTML, 'utf8');
+};
 
 export default compiler;
