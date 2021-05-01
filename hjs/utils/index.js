a0570a367c040593b568131ca8d1508681c2ddce
mikoloism <mikoloism.github@gmail.com>
2021-05-02 01:27:03 +0430

    Draft old Utils/

---
+++ hjs/utils/index.js
@@ -0,0 +1,19 @@
+// import _ from 'lodash';
+// import { html } from '../dom';
+
+const utils = {
+	// TODO: create children flexible, when you didn't any child keyword (html, children, md, from,)
+	// 	toChildren(children) {
+	// 		if (_.isString(children)) {
+	// 			// convert string html template to array of element object
+	// 			return [];
+	// 		}
+	// 		if (_.isArray(children)) {
+	// 			return children;
+	// 		}
+	// 		if (_.isObject(children)) {
+	// 			return html.isElement(children) ? children : render(children);
+	// 		}
+	// 	},
+};
+export default utils;
